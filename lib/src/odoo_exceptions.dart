06cd97eaf5e60a9f2291464699ca06603a4452ae
Yufeng <guo_yufeng@outlook.com>
2024-03-19 13:27:03 +0800

    remove debug from map

--- lib/src/odoo_exceptions.dart
+++ lib/src/odoo_exceptions.dart
@@ -34,24 +34,20 @@ class OdooSessionExpiredException implements Exception {
 
 class OdooExceptionErrorData {
   OdooExceptionErrorData({
-    required this.debug,
     required this.message,
     required this.name,
   });
 
   factory OdooExceptionErrorData.fromJson(Map<String, dynamic> json) =>
       OdooExceptionErrorData(
-        debug: json['debug'],
         message: json['message'],
         name: json['name'],
       );
 
-  final String debug;
   final String message;
   final String name;
 
   Map<String, dynamic> toJson() => {
-        'debug': debug,
         'message': message,
         'name': name,
       };
